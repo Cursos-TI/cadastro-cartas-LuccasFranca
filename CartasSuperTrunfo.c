b21d80eab171c4a1a4aec22cf3fd2be291b423db
Lucas França <133548447+LuccasFranca@users.noreply.github.com>
2025-09-19 19:47:29 +0000

    carta1 feita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,36 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char cidade[50];
+  int populacao, turismo;
+  float km, pib;
 
   // Área para entrada de dados
+  printf("Bem-Vindo ao Super Trunfo! \n");
+  
+  printf("Digite o nome da Cidade: \n");
+  scanf(" %[^\n]", cidade);
 
+  printf("Digite o numero aproximado da população: \n");
+  scanf("%d", &populacao);
+
+  printf("Digite quantos Km aproximadamente tem a cidade: \n");
+  scanf("%f", &km);
+
+  printf("Digite quantos pontos turisticos a cidade tem: \n");
+  scanf("%d", &turismo);
+
+  printf("Digite o PIB aproximado da cidade: \n");
+  scanf("%f", &pib);
+  
   // Área para exibição dos dados da cidade
+  printf("DADOS DA CIDADE\n");
+  printf("Cidade: %s\n", cidade);
+  printf("Populacao: %d\n", populacao);
+  printf("Km: %f\n", km);
+  printf("Pontos turisticos: %d\n", turismo);
+  printf("PIB: %f Bilhoes\n", pib);
 
+  
 return 0;
 } 
